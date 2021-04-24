69a1300373df57fe893099ec63f52a22c3e1496e
Patrick Furbert <patrickmfurbert@gmail.com>
2021-04-24 18:34:27 +0000

    updated menu.html with code for drop down nav and menu.css for styles

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # mrs-ds-empanadas
-Static HTML webpage for Local Empanada Shop
+Static HTML webpage for Local Empanada Shop in Rochester NY
--- js/menu.js
+++ js/menu.js
@@ -1,23 +1,25 @@
 // apply toggle open class for hamburger_wrapper span
 let hamburger_icon = document.getElementById("hamburger_wrapper")
 
+// when the hamburger icon is clicked by user the open class is toggled
 hamburger_icon.onclick = () =>{
     hamburger_icon.classList.toggle("open")
 }
 
-
-// display reel
+// display reel and picture index variable
 let pictures = document.getElementsByClassName("reel_imgs")
 let picture_index = 0;
 
+// function for cycling css class descriptor on photo
 function cycle_pictures() {
 
-
+    // loop through pictures
     for(x=0; x<pictures.length; x++){
-        //set all the pictures class to display none .hide
+        // set all the pictures class to display none .hide
         pictures[x].classList.add("hide")
     }
 
+    //
     pictures[picture_index++].classList.remove("hide")
 
     if(picture_index == pictures.length){
@@ -25,4 +27,5 @@ function cycle_pictures() {
     }
 }
 
+//set function to fire every 4000ms
 setInterval(cycle_pictures, 4000)
