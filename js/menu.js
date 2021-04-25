a6e53afc5773ba242a44924243a5e239033767c6
Patrick Furbert <patrickmfurbert@gmail.com>
2021-04-25 05:13:22 +0000

    fixed scrolling underneath menu

--- js/menu.js
+++ js/menu.js
@@ -1,12 +1,14 @@
-//get references to hamburger and drop down menu
+//get references to hamburger, drop down menu, and body
 let drop_down_menu = document.getElementById("drop_down_container")
 let hamburger_icon = document.getElementById("hamburger_wrapper")
+
 
 // apply toggle open class for hamburger_wrapper span
 // when the hamburger icon is clicked by user the open class is toggled
 hamburger_icon.onclick = () =>{
     hamburger_icon.classList.toggle("open")
     drop_down_menu.classList.toggle("show_drop_down_container")
+    document.body.classList.toggle("stop_body_scroll")
 
     //min-height needs to toggle between 12rem and 100vh on #navigation
     //display needs to change from flex to unset ??
